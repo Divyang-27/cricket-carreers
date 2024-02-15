1e61fb3743f922b98244067aa7da1d29e144b26f
Divyang Chhelavda <divyang.chhelavda.02@gmail.com>
2024-02-15 20:30:48 +0530

    Update player.js

--- Backend/controllers/player.js
+++ Backend/controllers/player.js
@@ -1,23 +1,41 @@
 const Player = require('../models/player');
 
 exports.postPlayer = async (req, res, next) => {
-  const player = await Player.create({
-   ...req.body
-  });
-  res.json({ newPlayerDetails: player });
+  try {
+    const player = await Player.create({
+      ...req.body,
+    });
+    res.json({ newPlayerDetails: player });
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
+
 exports.getPlayer = async (req, res, next) => {
-  const players = await Player.findAll();
-  res.send({ allPlayerDetails: players });
+  try {
+    const players = await Player.findAll();
+    res.send({ allPlayerDetails: players });
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 exports.getfindPlayer = async (req, res, next) => {
-  const name = req.params.playerName;
-  const player = await Player.findOne({ where: { name: name } });
-  return res.send({ playerDetails: player });
+  try {
+    const name = req.params.playerName;
+    const player = await Player.findOne({ where: { name: name } });
+    return res.send({ playerDetails: player });
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 exports.deletePlayer = async (req, res, next) => {
-  const id = req.params.playerId;
-  Player.destroy({ where: { id: id } });
+  try {
+    const id = req.params.playerId;
+    await Player.destroy({ where: { id: id } });
+    res.send({ message: 'Player deleted successfully' });
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
